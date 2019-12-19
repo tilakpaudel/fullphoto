3511fdfe8fe4266562af26c7a3f8adfebd68c581
tilakpaudel <tilakpaudel99.tp@gmail.com>
2019-12-19 16:21:33 +0900

    new updates for add picture

--- app/src/main/java/com/example/camerapic/MainActivity.java
+++ app/src/main/java/com/example/camerapic/MainActivity.java
@@ -29,10 +29,6 @@ public class MainActivity extends Activity {
     private static final int CAMERA_REQUEST = 1888;
     private static final int RESULT_LOAD_IMAGE = 999;
     private static final int MY_CAMERA_PERMISSION_CODE = 100;
-    private static String mFileName = null;
-    private Button startbtn, stopbtn, playbtn, stopplay;
-    private MediaRecorder mRecorder;
-    private MediaPlayer mPlayer;
     private ImageView imageView;
     boolean fullScreen = false;
 
@@ -40,15 +36,6 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startbtn = findViewById(R.id.btnRecord);
-        stopbtn = findViewById(R.id.btnStop);
-        playbtn = findViewById(R.id.btnPlay);
-        stopplay = findViewById(R.id.btnStopPlay);
-        stopbtn.setEnabled(false);
-        playbtn.setEnabled(false);
-        stopplay.setEnabled(false);
-        mFileName = getExternalCacheDir().getAbsolutePath();
-        mFileName += "/AudioRecording.3gp";
         this.imageView = this.findViewById(R.id.imageView);
 
         imageView.setOnClickListener(new View.OnClickListener() {
@@ -98,71 +85,6 @@ public class MainActivity extends Activity {
                     android.Manifest.permission.RECORD_AUDIO,
                     android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_AUDIO_PERMISSION_CODE);
         }
-
-        startbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                stopbtn.setEnabled(true);
-                startbtn.setEnabled(false);
-                playbtn.setEnabled(false);
-                stopplay.setEnabled(false);
-                mRecorder = new MediaRecorder();
-                mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-                mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-                mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-                mRecorder.setOutputFile(mFileName);
-                try {
-                    mRecorder.prepare();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                mRecorder.start();
-            }
-        });
-
-        stopbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                stopbtn.setEnabled(false);
-                startbtn.setEnabled(true);
-                playbtn.setEnabled(true);
-                stopplay.setEnabled(true);
-                mRecorder.stop();
-                mRecorder.release();
-                mRecorder = null;
-            }
-        });
-
-        playbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                stopbtn.setEnabled(false);
-                startbtn.setEnabled(true);
-                playbtn.setEnabled(false);
-                stopplay.setEnabled(true);
-                mPlayer = new MediaPlayer();
-                try {
-                    mPlayer.setDataSource(mFileName);
-                    mPlayer.prepare();
-                    mPlayer.start();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        stopplay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mPlayer.release();
-                mPlayer = null;
-                stopbtn.setEnabled(false);
-                startbtn.setEnabled(true);
-                playbtn.setEnabled(true);
-                stopplay.setEnabled(false);
-            }
-        });
     }
 
     @Override
